b90f4bfdcd0af94758c9040b3c681514ea0ee3ae
Alex-GT <isc.agt@gmail.com>
2017-09-29 15:20:55 -0500

    Tabla Programas actualizados

--- insezacweb/view/programa/index.php
+++ insezacweb/view/programa/index.php
@@ -1,71 +1,71 @@
-
-<div class="row">
-  <div class="col-md-12">
-    <div class="block-web">
-      <div class="header">
-        <div class="actions"> </div>
-        <h3 class="content-header">Programas</h3>
-      </div>
-      <div class="porlets-content">
-        <div class="adv-table editable-table ">
-          <div class="clearfix">
+<div class="pull-left breadcrumb_admin clear_both">
+  <div class="pull-left page_title theme_color">
+    <h1>Inicio</h1>
+    <h2 class="">Programas</h2>
+  </div>
+  <div class="pull-right">
+    <ol class="breadcrumb">
+      <li><a href="?c=Inicio">Inicio</a></li>
+      <li class="active">Programas</a></li>
+    </ol>
+  </div>
+</div>
+<div class="container clear_both padding_fix">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="invoice_header">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="input-group">
+              <input type="text" class="form-control">
+              <span class="input-group-btn">
+                <button type="button" class="btn btn-default"><i class="fa fa-search"></i> Buscar </button>
+              </span>
+            </div>
           </div>
-          <div class="margin-top-10"></div>
-          <table class="table table-striped table-hover table-bordered" id="editable-sample">
-            <thead>
-              <tr>
-                <th>Nombre del programa</th>
-                <th>Cantidad de subprogramas</th>
-                <th>Edit</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="">
-                <td>Emprendedores en las tecnologias</td>
-                <td>10</td>
-                <td class="center"><a class="btn btn-warning">Editar<i class="fa fa-edit"></i></a></td>
-                <td class="center"><a class="btn btn-danger">Borrar<i class="fa fa-eraser"></i></a></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div><!--/porlets-content-->
-    </div><!--/block-web-->
-  </div><!--/col-md-12-->
-</div><!--/row-->
-<div class="row">
-  <div class="col-md-12">
-    <div class="block-web">
-      <div class="header">
-        <div class="actions"> </div>
-        <h3 class="content-header">Programas</h3>
-      </div>
-      <div class="porlets-content">
-        <div class="adv-table editable-table ">
-          <div class="clearfix">
+          <div class="col-sm-8">
+            <div class="btn-group pull-right">
+              <b>
+                <div class="btn-group"> <a class="btn btn-sm btn-success" href="?c=Programa&a=Alta"> <i class="fa fa-plus"></i> Registrar programa </a> </div>
+              </b>
+            </div>
           </div>
-          <div class="margin-top-10"></div>
-          <table class="table table-striped table-hover table-bordered" id="editable-sample">
-            <thead>
-              <tr>
-                <th>Nombre del programa</th>
-                <th>Cantidad de subprogramas</th>
-                <th>Edit</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="">
-                <td>Emprendedores en las tecnologias</td>
-                <td>10</td>
-                <td class="center"><a class="btn btn-warning">Editar<i class="fa fa-edit"></i></a></td>
-                <td class="center"><a class="btn btn-danger">Borrar<i class="fa fa-eraser"></i></a></td>
-              </tr>
-            </tbody>
-          </table>
         </div>
-      </div><!--/porlets-content-->
-    </div><!--/block-web-->
-  </div><!--/col-md-12-->
-</div><!--/row-->
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="block-web">
+        <div class="header">
+          <div class="actions"> </div>
+          <h3 class="content-header">Programas</h3>
+        </div>
+        <div class="porlets-content">
+          <div class="adv-table editable-table ">
+            <div class="clearfix">
+            </div>
+            <div class="margin-top-10"></div>
+            <table class="table table-striped table-hover table-bordered" id="editable-sample">
+              <thead>
+                <tr>
+                  <th>Nombre del programa</th>
+                  <th>Cantidad de subprogramas</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="">
+                  <td>Emprendedores en las tecnologias</td>
+                  <td>10</td>
+                  <td class="center"><a class="btn btn-warning">Editar<i class="fa fa-edit"></i></a></td>
+                  <td class="center"><a class="btn btn-danger">Borrar<i class="fa fa-eraser"></i></a></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div><!--/porlets-content-->
+      </div><!--/block-web-->
+    </div><!--/col-md-12-->
+  </div><!--/row-->
