3ae0eb81b9defc5bc22737df3b5ca0b50da12f49
Alex-GT <isc.agt@gmail.com>
2017-09-29 15:19:14 -0500

    Tabla Programas actualizados

--- insezacweb/view/programa/index.php
+++ insezacweb/view/programa/index.php
@@ -1,36 +1,71 @@
 
-            <div class="row">
-              <div class="col-md-12">
-                <div class="block-web">
-                  <div class="header">
-                    <div class="actions"> </div>
-                    <h3 class="content-header">Programas</h3>
-                  </div>
-                  <div class="porlets-content">
-                    <div class="adv-table editable-table ">
-                      <div class="clearfix">
-                      </div>
-                      <div class="margin-top-10"></div>
-                      <table class="table table-striped table-hover table-bordered" id="editable-sample">
-                        <thead>
-                          <tr>
-                            <th>Nombre del programa</th>
-                            <th>Cantidad de subprogramas</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr class="">
-                            <td>Emprendedores en las tecnologias</td>
-                            <td>10</td>
-                            <td class="center"><a class="btn btn-warning">Editar<i class="fa fa-edit"></i></a></td>
-                            <td class="center"><a class="btn btn-danger">Borrar<i class="fa fa-eraser"></i></a></td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div><!--/porlets-content-->
-                </div><!--/block-web-->
-              </div><!--/col-md-12-->
-            </div><!--/row-->
+<div class="row">
+  <div class="col-md-12">
+    <div class="block-web">
+      <div class="header">
+        <div class="actions"> </div>
+        <h3 class="content-header">Programas</h3>
+      </div>
+      <div class="porlets-content">
+        <div class="adv-table editable-table ">
+          <div class="clearfix">
+          </div>
+          <div class="margin-top-10"></div>
+          <table class="table table-striped table-hover table-bordered" id="editable-sample">
+            <thead>
+              <tr>
+                <th>Nombre del programa</th>
+                <th>Cantidad de subprogramas</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="">
+                <td>Emprendedores en las tecnologias</td>
+                <td>10</td>
+                <td class="center"><a class="btn btn-warning">Editar<i class="fa fa-edit"></i></a></td>
+                <td class="center"><a class="btn btn-danger">Borrar<i class="fa fa-eraser"></i></a></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div><!--/porlets-content-->
+    </div><!--/block-web-->
+  </div><!--/col-md-12-->
+</div><!--/row-->
+<div class="row">
+  <div class="col-md-12">
+    <div class="block-web">
+      <div class="header">
+        <div class="actions"> </div>
+        <h3 class="content-header">Programas</h3>
+      </div>
+      <div class="porlets-content">
+        <div class="adv-table editable-table ">
+          <div class="clearfix">
+          </div>
+          <div class="margin-top-10"></div>
+          <table class="table table-striped table-hover table-bordered" id="editable-sample">
+            <thead>
+              <tr>
+                <th>Nombre del programa</th>
+                <th>Cantidad de subprogramas</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="">
+                <td>Emprendedores en las tecnologias</td>
+                <td>10</td>
+                <td class="center"><a class="btn btn-warning">Editar<i class="fa fa-edit"></i></a></td>
+                <td class="center"><a class="btn btn-danger">Borrar<i class="fa fa-eraser"></i></a></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div><!--/porlets-content-->
+    </div><!--/block-web-->
+  </div><!--/col-md-12-->
+</div><!--/row-->
