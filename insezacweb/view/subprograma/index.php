feb02689fffdc941118c0c8c041bd0c213475272
Alex-GT <isc.agt@gmail.com>
2017-09-29 15:09:10 -0500

    Tabla Programas

---
+++ insezacweb/view/subprograma/index.php
@@ -0,0 +1,174 @@
+<div class="pull-left breadcrumb_admin clear_both">
+ <div class="pull-left page_title theme_color">
+   <h1>Inicio</h1>
+   <h2 class="">Programas</h2>
+ </div>
+ <div class="pull-right">
+   <ol class="breadcrumb">
+     <li><a href="?c=Inicio">Inicio</a></li>
+     <li class="active">Programas</a></li>
+   </ol>
+ </div>
+</div>
+<div class="container clear_both padding_fix">
+ <div class="row">
+   <div class="col-md-12">
+     <div class="invoice_header">
+       <div class="row">
+         <div class="col-sm-4">
+           <div class="input-group">
+             <input type="text" class="form-control">
+             <span class="input-group-btn">
+               <button type="button" class="btn btn-default"><i class="fa fa-search"></i> Buscar </button>
+             </span>
+           </div>
+         </div>
+         <div class="col-sm-8">
+           <div class="btn-group pull-right">
+             <b>
+               <div class="btn-group"> <a class="btn btn-sm btn-success" href="?c=Programa&a=Alta"> <i class="fa fa-plus"></i> Registrar programa </a> </div>
+             </b>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
+
+</div>
+
+
+<div class="container clear_both padding_fix">
+ <div class="row">
+   <div class="col-md-12">
+     <div class="block-web">
+       <div class="row">
+         <div class="col-md-6">
+           <a href="#" class="open_ticket_comment">
+             <div class="open_ticket_thumnail">
+               <img src="assets/images/programa1.jpg" alt="" style="max-width: 80px; max-height: 80px; margin-top: 5px; margin-left: 10px; margin-right: 20px;">
+
+             </div>
+             <div class="ticket_problem" style="margin-top: 10px;">Programa 1</div>
+             <span>Descripción de programa 1</span>
+             <p><b>Periodo:</b> 29/09/2017 - 20/09/2018</p>
+             <div class="ticket_action" style="margin-top: -45px;">
+               <div class="ticket_action_view">i</div>
+             </div>
+           </a>
+         </div>
+         <div class="col-md-6">
+           <a href="#" class="open_ticket_comment">
+             <div class="open_ticket_thumnail">
+               <img src="assets/images/programa2.jpg" alt="" style="max-width: 80px; max-height: 80px; margin-top: 5px; margin-left: 10px; margin-right: 20px;">
+
+             </div>
+             <div class="ticket_problem" style="margin-top: 10px;">Programa 2</div>
+             <span>Descripción de programa 2</span>
+             <p><b>Periodo:</b> 29/09/2017 - 20/09/2018</p>
+             <div class="ticket_action" style="margin-top: -45px;">
+               <div class="ticket_action_view">i</div>
+             </div>
+           </a>
+         </div>
+         <div class="col-md-6">
+           <a href="#" class="open_ticket_comment">
+             <div class="open_ticket_thumnail">
+               <img src="assets/images/programa3.jpg" alt="" style="max-width: 80px; max-height: 80px; margin-top: 5px; margin-left: 10px; margin-right: 20px;">
+
+             </div>
+             <div class="ticket_problem" style="margin-top: 10px;">Programa 3</div>
+             <span>Descripción de programa 3</span>
+             <p><b>Periodo:</b> 29/09/2017 - 20/09/2018</p>
+             <div class="ticket_action" style="margin-top: -45px;">
+               <div class="ticket_action_view">i</div>
+             </div>
+           </a>
+         </div>
+         <div class="col-md-6">
+           <!--div class="ticket_open"-->
+           <a href="#" class="open_ticket_comment">
+             <div class="open_ticket_thumnail">
+               <img src="assets/images/programa4.png" alt="" style="max-width: 80px; max-height: 80px; margin-top: 5px; margin-left: 10px; margin-right: 20px;">
+
+             </div>
+             <div class="ticket_problem" style="margin-top: 10px;">Programa 4</div>
+             <span>Descripción de programa 4</span>
+             <p><b>Periodo:</b> 29/09/2017 - 20/09/2018</p>
+             <div class="ticket_action" style="margin-top: -45px;">
+               <div class="ticket_action_view">i</div>
+             </div>
+           </a>
+           <!--/div-->
+         </div>
+       </div>
+     </div><!--/block-web-->
+   </div><!--/col-md-12-->
+ </div><!--/row-->
+</div><!--/container clear_both padding_fix-->
+
+
+
+
+
+<div class="container clear_both padding_fix">
+ <div class="row">
+   <div class="col-md-12">
+     <a href="#" class="ticket_open_comment">
+       <div class="btn-group"><i class="fa fa-user"></i></div>
+       <span>Programa 1</span>
+       <p>Descripción de programa 1</p>
+       <div class="ticket_action" style="margin-top: -35px;">
+         <div class="ticket_action_view">i</div>
+       </div>
+     </a>
+   </div>
+ </div>
+</div>
+<div class="container clear_both padding_fix">
+ <div class="row">
+   <div class="col-md-12">
+     <a href="#" class="ticket_open_comment">
+       <div class="btn-group"><i class="fa fa-user"></i></div>
+       <span>Programa 2</span>
+       <p>Descripción de programa 1</p>
+       <div class="ticket_action" style="margin-top: -35px;">
+         <div class="ticket_action_view">i</div>
+       </div>
+     </a>
+   </div>
+ </div>
+</div>
+
+<div class="container clear_both padding_fix">
+ <div class="row">
+   <div class="col-md-12">
+     <div class="block-web">
+       <div class="header">
+         <div class="actions"> <a class="minimize" href="#"><i class="fa fa-chevron-down"></i></a><a class="close-down" href="#"><i class="fa fa-times"></i></a> </div>
+         <h3>Programa 1</h3>
+       </div>
+       <div class="porlets-content">
+         <p>Descripción de programa 1</p>
+       </div><!--/porlets-content-->
+     </div><!--/block-web-->
+   </div><!--/col-md-12-->
+ </div><!--/row-->
+</div><!--/container clear_both padding_fix-->
+
+
+<div class="container clear_both padding_fix">
+ <div class="row">
+   <div class="col-md-12">
+     <div class="block-web">
+       <div class="header">
+         <div class="actions"> <a class="minimize" href="#"><i class="fa fa-chevron-down"></i></a><a class="close-down" href="#"><i class="fa fa-times"></i></a> </div>
+         <h3>Programa 2</h3>
+       </div>
+       <div class="porlets-content">
+         <p>Descripción de programa 2</p>
+       </div><!--/porlets-content-->
+     </div><!--/block-web-->
+   </div><!--/col-md-12-->
+ </div><!--/row-->
+</div><!--/container clear_both padding_fix-->
