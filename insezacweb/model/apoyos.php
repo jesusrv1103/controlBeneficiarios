5192938b6e32c3f66686c29dcb5617ea4e453534
Jesus <jesus21c.jrv@gmail.com>
2017-10-03 17:57:54 -0500

    Cambios  enprogramas

--- insezacweb/model/apoyos.php
+++ insezacweb/model/apoyos.php
@@ -1,7 +1,17 @@
 <?php
-class apoyos
+class Apoyos
 {
-	private $pdo;
+	private $pdo; //int 
+	public $idOrigen; //int
+	public $idPrograma; //int
+	public $idSubprograma;  //int
+	public $idTipoApoyo;  //int
+	public $idCaracteristicas; //int
+	public $importeApoyo; //int
+	public $numerosApoyo;  //int
+	public $fechaultimoApoyo; //Date
+	public $idPeriodicidad;  //int
+
 	public function __CONSTRUCT()
 	{
 		try
@@ -13,4 +23,95 @@ class apoyos
 			die($e->getMessage());
 		}
 	}
+
+		//Metdodo para listar
+		public function Listar($nomPrograma)
+		{
+			try
+			{
+				
+	
+				$stm = $this->pdo->prepare("SELECT * from apoyos");
+				
+				$stm->execute($nomPrograma);
+	
+				return $stm->fetchAll(PDO::FETCH_OBJ);
+			}
+			catch(Exception $e)
+			{
+				die($e->getMessage());
+			}
+		}
+	
+		public function Obtener($id)
+		{
+			try 
+			{
+				$stm = $this->pdo
+						  ->prepare("SELECT * FROM programa WHERE idPrograma = ?");
+						  
+	
+				$stm->execute(array($id));
+				return $stm->fetch(PDO::FETCH_OBJ);
+			} catch (Exception $e) 
+			{
+				die($e->getMessage());
+			}
+		}
+		// select * from programa WHERE programa like '%TRA%';
+		
+		public function Eliminar($id)
+		{
+			try 
+			{
+				$stm = $this->pdo
+							->prepare("DELETE FROM programa WHERE idPrograma = ?");			          
+	
+				$stm->execute(array($id));
+			} catch (Exception $e) 
+			{
+				die($e->getMessage());
+			}
+		}
+	
+		//Metodo para actualizar
+		public function Actualizar($data)
+		{
+			try 
+			{
+				$sql = "UPDATE programa SET 
+							programa = ?
+						WHERE idPrograma = ?";
+	
+				$this->pdo->prepare($sql)
+					 ->execute(
+						array(
+							$data->programa, 
+							$data->idPrograma
+						)
+					);
+			} catch (Exception $e) 
+			{
+				die($e->getMessage());
+			}
+		}
+		//Metdod para registrar
+		public function Registrar(Programa $data)
+		{
+			try 
+			{
+			$sql = "INSERT INTO programa (programa) 
+					VALUES (?)";
+	
+			$this->pdo->prepare($sql)
+				 ->execute(
+					array(
+						$data->programa
+					)
+				);
+			} catch (Exception $e) 
+			{
+				die($e->getMessage());
+			}
+		}
 }
