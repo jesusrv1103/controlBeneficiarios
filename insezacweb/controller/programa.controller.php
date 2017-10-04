0ecfcd0e0dadad4d4699dc926d20d307fbcfd0c5
Alejandro Castro Saucedo <Alejandro Castro Saucedo>
2017-10-03 22:27:38 -0500

    Programas

--- insezacweb/controller/programa.controller.php
+++ insezacweb/controller/programa.controller.php
@@ -4,55 +4,49 @@ require_once 'model/programa.php';
 class ProgramaController{
 
   private $model;
-  //private $session;
   public $error;
 
   //Constructor
   public function __CONSTRUCT(){
     $this->model = new Programa();
   }
-
-
   //Index 
   public function Index(){
-
-
-   $page="view/programa/index.php";
+    $administracion=true; //variable cargada para activar la opcion administracion en el menu
+    $programas=true; //variable cargada para activar la opcion programas en el menu
+   $page="view/programa/index.php"; //Vista principal donde se enlistan los programas
    require_once 'view/index.php';
- 	} 
-
-
+ } 
  	//Metodo Crud *** Mandara llamar a Metodo Crear select editar y eliminar
-  public function Crud(){
-  	$programa = new Programa();
-    
-
-    if(isset($_REQUEST['idPrograma'])){
-            $programa = $this->model->Obtener($_REQUEST['idPrograma']);
-        }
-        $page= "view/programa/programa.php";
-       require_once 'view/index.php';
-  } 
-
-  //Metodo Guardar  si trae un id  actualiza si no registra
- 	public function Guardar(){
-        $programa= new Programa();
+ public function Crud(){
+   $programa = new Programa();
+   if(isset($_REQUEST['idPrograma'])){
+    $programa = $this->model->Obtener($_REQUEST['idPrograma']);
+  }
+  $page= "view/programa/programa.php";
+  require_once 'view/index.php';
+} 
+  //Metodo Guardar  si trae un id actualiza, no registra
+public function Guardar(){
+  $programa= new Programa();
         //echo "entre";
         //echo  $programa->idPrograma
-        $programa->idPrograma = $_REQUEST['idPrograma'];
-        $programa->programa = $_REQUEST['programa'];
-        $programa->idPrograma > 0 
-            ? $this->model->Actualizar($programa)
-            : $this->model->Registrar($programa);
-        $page="view/programa/index.php";
-   			require_once 'view/index.php';
-    }
+  $programa->idPrograma = $_REQUEST['idPrograma'];
+  $programa->programa = $_REQUEST['programa'];
+  $programa->idPrograma > 0 
+  ? $this->model->Actualizar($programa)
+  : $this->model->Registrar($programa);
+  $administracion=true;
+  $programas=true;
+  $page="view/programa/index.php";
+  require_once 'view/index.php';
+}
     //Metodo  para eliminar
-    public function Eliminar(){
-        $this->model->Eliminar($_REQUEST['idPrograma']);
-        echo $idPrograma;
-   			$page="view/programa/index.php";
-   			require_once 'view/index.php';
-    }
+public function Eliminar(){
+  $this->model->Eliminar($_REQUEST['idPrograma']);
+  echo $idPrograma;
+  $page="view/programa/index.php";
+  require_once 'view/index.php';
+}
 
 }
