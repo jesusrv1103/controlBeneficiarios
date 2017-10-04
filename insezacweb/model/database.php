93183f9b8d0dc6858dc42bd29872545c72c8c618
Alejandro Castro Saucedo <Alejandro Castro Saucedo>
2017-10-04 14:30:02 -0500

    VERSION 1.0

--- insezacweb/model/database.php
+++ insezacweb/model/database.php
@@ -16,7 +16,7 @@ class Database
 				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
 				return $pdo;
 			}else{
-				$pdo = new PDO('mysql:host=localhost;dbname=INSEZAC;charset=utf8', 'root', '123-horses');
+				$pdo = new PDO('mysql:host=localhost;dbname=INSEZAC;charset=utf8', 'root', '');
 				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
 				return $pdo;
 			}
