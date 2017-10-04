b6bdc332131156d829c9d84b0fb0593bad09e7b8
Jesus <jesus21c.jrv@gmail.com>
2017-10-04 09:29:40 -0500

    Commit para hacer pull

--- insezacweb/model/programa.php
+++ insezacweb/model/programa.php
@@ -23,10 +23,7 @@ class Programa
 	{
 		try
 		{
-			//$result = array();
-		    // SELECT p.idPrograma, p.programa, sum(techoPresupuestal) as suma   FROM programa p , subprograma s where p.idPrograma =  s.idPrograma group by p.idPrograma
-
-			$stm = $this->pdo->prepare("SELECT * from programa");
+			
 			
 			$stm->execute();
 
