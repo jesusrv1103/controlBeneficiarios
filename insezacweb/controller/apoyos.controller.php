ab2b264635ae419c81257b1124836b720bb0ce85
Julieta_Ibarra <juliet_1003@hotmail.com>
2017-11-12 15:24:10 -0600

    Realice el crud de Apoyos

--- insezacweb/controller/apoyos.controller.php
+++ insezacweb/controller/apoyos.controller.php
@@ -16,19 +16,53 @@ class ApoyosController{
    $page="view/apoyos/index.php";
    require_once 'view/index.php';
  }
- public function Alta(){
+ public function Crud(){
+  if(isset($_REQUEST['nuevoRegistro'])){
+    $nuevoRegistro=true;
+  }
+  $apoyo = new Apoyo();
+  if(isset($_REQUEST['idApoyo'])){
+    $apoyo = $this->model->Obtener($_REQUEST['idApoyo']);
+  }
   $administracion = true;
   $inicio = false;
   $beneficiarios = false;
   $page="view/apoyos/apoyos.php";
   require_once 'view/index.php';
 }
-public function Create(){
+public function Eliminar(){
+  $this->model->Eliminar($_REQUEST['idApoyo']);
+  $administracion = true;
+  $apoyos = true;
+  $page="view/apoyos/index.php";
+  $mensaje="Se ha eliminado correctamente el apoyo";
+  require_once 'view/index.php';
 }
-public function Update(){
-
+public function Guardar(){
+  $apoyo= new Apoyos();
+  $apoyo->idApoyo = $_REQUEST['idApoyo'];
+  $apoyo->curp = $_REQUEST['curp'];
+  $apoyo->idOrigen = $_REQUEST['idOrigen'];
+  $apoyo->idPrograma = $_REQUEST['idPrograma'];
+  $apoyo->idSubprograma = $_REQUEST['idSubprograma'];
+  $apoyo->idTipoApoyo = $_REQUEST['idTipoApoyo'];
+  $apoyo->idCaracteristica = $_REQUEST['idCaracteristica'];
+  $apoyo->importeApoyo = $_REQUEST['importeApoyo'];
+  $apoyo->numeroApoyo = $_REQUEST['numeroApoyo'];
+  $apoyo->fechaUltimoApoyo = $_REQUEST['fechaUltimoApoyo'];
+  $apoyo->idPeriodicidad = $_REQUEST['idPeriodicidad'];
+  $apoyo->apoyoEconomico = $_REQUEST['apoyoEconomico'];
+  if(!isset($_REQUEST['nuevoRegistro'])){
+    $this->model->Actualizar($apoyo);
+    $mensaje="Se han actualizado correctamente los datos del Apoyo";
+  }else{
+    $this->model->Registrar($apoyo);
+    $mensaje="Se han registrado correctamente los datos del Apoyo";
+  } 
+  $administracion = true;
+  $apoyos = true;
+  $page="view/apoyos/index.php";
+  require_once 'view/index.php';
 }
-public function Delete(){
-
 }
 }
