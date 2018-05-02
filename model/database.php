585199590ff2e8584e1a16f0ec986cbac7dae507
jesusrv1103 <jesus21c.jrv@gmail.com>
2018-05-02 18:24:06 -0500

    IMPORTAR CORREGIDOS

--- model/database.php
+++ model/database.php
@@ -5,21 +5,12 @@ class Database
 	{
 		try
 		{
-			if (!isset($_SESSION['seguridad']) || $_SESSION['tipoUsuario']==1) {
+			
+			$pdo = new PDO('mysql:host=srv-isp3.zacatecas.gob.mx;dbname=c4insezac;charset=utf8', 'c4insezac', 'JjbLc55!');
 
-				$pdo = new PDO('mysql:host=srv-isp3.zacatecas.gob.mx;dbname=c4insezac;charset=utf8', 'c4insezac', 'JjbLc55!');
-
-				//$pdo = new PDO('mysql:host=localhost;dbname=insezac;charset=utf8', 'root', '');
-
-				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				return $pdo;
-			}else{
-				$usuario=$_SESSION['usuario'];
-				$password=$_SESSION['password'];
-				$pdo = new PDO('mysql:host=srv-isp3.zacatecas.gob.mx;dbname=c4insezac;charset=utf8',$usuario,$password);
-				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				return $pdo;
-			}
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $pdo;
+			
 		}
 		catch(PDOException $e)
 		{
